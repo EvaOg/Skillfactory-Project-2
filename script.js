13335eb0fb7ea6bbaedf94252ba05c83fed1103c
Evvaog <evvaog@gmail.com>
2023-08-16 09:15:30 +0200

    Corrected mobile version & section 2

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
+let currentDot = 0;
+let currentName = 0;
 let prevslide;
 
 const back = document.querySelector('.prev')
@@ -25,7 +27,6 @@ back.addEventListener('click', () => {
   const navPatriotic = document.querySelector("#patriotic")
 
 
-
   let navs = [navAdmiral, navThieves, navPatriotic];
   const dots = document.querySelectorAll('.dot');
   for(let i = 0; i < navs.length; i++) {
@@ -34,17 +35,29 @@ back.addEventListener('click', () => {
           for(let slide of slides) {
             slide.classList.remove("active");
           }
+          for (let el of navs) {
+            el.classList.remove("activeName")
+          }
           currentSlide = i;
           slides[i].classList.add("active");
+          navs[i].classList.add("activeName"); 
     });
 
     let dot = dots[i];
+
     dot.addEventListener("click", () => {
         for(let slide of slides) {
           slide.classList.remove("active");
         }
+
+        for (let dot of dots) {
+          dot.classList.remove("activeDot")
+        }
+
         currentSlide = i;
+        currentDot = i;
         slides[i].classList.add("active");
+        dots[i].classList.add("activeDot");  
   });
   }
 
